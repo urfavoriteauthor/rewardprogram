9ea074aa2544b2775d3cb24a92212e473a6a05d9
Nygel Porterfield <nygel.porterfield@gmail.com>
2020-02-04 19:28:30 -0500

    first

---
+++ reward/src/components/menu.js
@@ -0,0 +1,17 @@
+import react from 'react'
+
+class Menu extends React.Component{
+    render(){
+        return(
+            <div>
+<div className='ul'>
+    <div className='li'><li><a href='#'>$25 Headphones</a></li></div>
+    <div className='li'><li><a href='#'>$50 Headphones</a></li></div>
+    <div className='li'><li><a href='#'>$75 Headphones</a></li></div>
+    <div className='li'><li><a href='#'>$100 Headphones</a></li></div>
+</div>
+            </div>
+        )
+    }
+}
+export default Menu;
---
+++ reward/src/main.js
@@ -0,0 +1,16 @@
+import react from 'react'
+import Menu from './components/menu'
+
+class Main extends React.Component{
+    render(){
+        return(
+<div>
+    <h1>Beats by Nygel</h1>
+    <h5>Sign up for Our Rewards Program</h5>
+    <Menu/>
+</div>
+        )
+    }
+}
+
+export default Main;
